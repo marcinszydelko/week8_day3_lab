8e0e77284b3af20cd0e2ec9e8a1a60064f53b5c6
Marcin <marcinszydelko@yahoo.com>
2019-12-04 14:03:31 +0000

    update form appears

--- client/src/components/GuestDetail.vue
+++ client/src/components/GuestDetail.vue
@@ -4,6 +4,22 @@
     <p>{{guest.email}}</p>
     <p>{{checkInStatus()}}</p>
     <button @click="buttonDelete" type="button" name="delete">Delete</button>
+    <button @click="buttonUpdate" type="button" name="update">Update</button>
+      <form v-if="showUpdate" v-on:submit="saveUpdate" >
+        <label for="name">Name:</label>
+        <input type="text" v-model="name" >
+
+        <label for="email">Email:</label>
+        <input type="email" v-model="email" >
+
+        <label for="checkedin">Checked in status:</label>
+        <select v-model="checkedin" >
+          <option value="">Checked in</option>
+          <option value="">Not checked in</option>
+        </select>
+        <input type="submit" value="Save">
+      </form>
+
   </div>
 </template>
 
@@ -11,6 +27,14 @@
 import {eventBus} from '../main.js'
 import  GuestService from '../services/GuestService.js'
 export default {
+  data(){
+    return{
+      name: "",
+      email: "",
+      checkedin: null,
+      showUpdate: false
+    }
+  },
   name: "GuestDetail",
   props: ['guest'],
   methods: {
@@ -24,10 +48,17 @@ export default {
     buttonDelete(){
       GuestService.deleteGuest(this.guest._id)
       .then(() => eventBus.$emit('guest-deleted', this.guest._id))
+    },
+    buttonUpdate(){
+      return this.showUpdate = true;
+    },
+    saveUpdate(){
+
     }
   }
 }
 </script>
 
 <style lang="css" scoped>
+  
 </style>
